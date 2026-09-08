e7b19d9cf85e94991fc2e5a6e26b91bb481e8c55
sharmayush11 <ayusharma1127@gmail.com>
2026-09-08 16:57:32 +0530

    learning express

--- app.js
+++ app.js
@@ -1,21 +1,17 @@
-const http = require("http");
-const server = http.createServer((req, res) => {
-    console.log(req.method);
-    console.log(req.url);
-    if (req.method === "POST" && req.url === "/users") {
-        let body = "";
-        req.on("data", (chunk) => {
-            body += chunk;
-        });
-        req.on("end", () => {
-            console.log("Request completed");
-            console.log(body); 
-            res.end("User received");
-        });
-    } else {
-        res.end("Not found");
-    }
+const express = require('express');
+
+const server = express();
+
+server.get("/users/:userId/posts/:postId", (req, res) => {
+    console.log(req.params);
+    const uId = req.params.userId
+    const pId = req.params.postId
+    console.log("User ID =",  uId);
+    console.log("Post ID =",  pId);
+    
+    res.send(`User ID: ${uId}, Post ID: ${pId}`);
 });
+
 server.listen(3000, () => {
-    console.log("Server started on port 3000");
+    console.log('server started on port 3000');
 });
