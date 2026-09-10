5252b45bf8fa78f36b9603878db5b796647694af
sharmayush11 <ayusharma1127@gmail.com>
2026-09-10 15:44:57 +0530

    learning basics of crud operations

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,19 +1,22 @@
-let users = [
-    {
-        id: 1,
-        name: "Ayush"
-    },
-    {
-        id: 2,
-        name: "Rahul"
-    }
-];
-const getUsers = (req, res) => {
-    res.status(200).json({
-        users: users
-    });
+const users = [
+    { id : 1 , name : 'ayu' },
+    { id : 2 , name : 'adi' },
+    { id : 3 , name : 'jai' }
+]
 
-};
-module.exports = {
-    getUsers
-};
+const deleteUser = (req , res) => {
+    const id = Number(req.params.id)
+    const userIdx = users.findIndex(user => user.id === id)
+
+    if (userIdx === -1) {
+        return res.status(404).json({
+            message : 'user not found'
+        })
+    }
+    users.splice(userIdx , 1)
+    res.status(200).json({
+        message : 'user deleted'
+    })
+}
+
+module.exports = { getUser , getUsers }
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,5 +1,6 @@
-const express = require("express");
-const router = express.Router();
-const { getUsers } = require("../controllers/userController");
-router.get("/", getUsers);
-module.exports = router;
+const express = require('express')
+const router = express.Router()
+const { getUsers,getUser } = require('../controllers/userController')
+router.get('/',getUsers)
+router.get('/:id',getUser)
+module.exports = router
