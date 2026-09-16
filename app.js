baf76e0ee3d6014f1b40dad42548af3f99cab128
sharmayush11 <ayusharma1127@gmail.com>
2026-09-16 18:17:22 +0530

    schema & model

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
+require("dotenv").config();
+const connectDB = require("./db");
+connectDB();
+////////
 const express = require("express");
 const app = express();
-
-// Normal route
-app.get("/test", (req, res, next) => {
-    const error = new Error("Something went wrong");
-    next(error);
-});
-
-
-// Error-handling middleware
-app.use((err, req, res, next) => {
-    console.log(err.message);
-    res.status(500).json({
-        message: "Internal Server Error"
-    });
-});
-
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log("Server started on port 3000");
 });
+
