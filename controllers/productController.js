49173b4d647ba729ee82d53bf67e58bb1ede9da9
sharmayush11 <ayusharma1127@gmail.com>
2026-09-22 23:00:43 +0530

    error handling

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,49 +1,31 @@
-const Product = require("../models/Product");
-
-// GET all products
-const getProducts = async (req, res) => {
-    const products = await Product.find();
-    res.json(products);
-};
-
-// GET single product
 const getProduct = async (req, res) => {
-    const product = await Product.findById(req.params.id);
-    res.json(product);
-};
+    try {
+        const product = await Product.findById(req.params.id);
 
-// POST create product
-const createProduct = async (req, res) => {
-    const product = await Product.create(req.body);
-    res.status(201).json(product);
-};
-
-const updateProduct = async (req, res) => {
-    const product = await Product.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        {
-            new: true,
-            runValidators: true
+        // Product nahi mila
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
         }
-    );
 
-    res.json(product);
-};
+        // Product mil gaya
+        res.status(200).json({
+            product: product
+        });
 
-const deleteProduct = async (req, res) => {
-    const product = await Product.findByIdAndDelete(req.params.id);
+    } catch (error) {
 
-    res.json({
-        message: "Product deleted successfully",
-        product
-    });
-};
+        // Invalid MongoDB ObjectId
+        if (error.name === "CastError") {
+            return res.status(400).json({
+                message: "Invalid product ID"
+            });
+        }
 
-module.exports = {
-    getProducts,
-    getProduct,
-    createProduct,
-    updateProduct,
-    deleteProduct
+        // Other unexpected errors
+        res.status(500).json({
+            message: "Internal server error"
+        });
+    }
 };
